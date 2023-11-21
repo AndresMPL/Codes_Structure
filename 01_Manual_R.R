81741e0dd84bac32a0c5a75065367cc3a75a3c5d
AndresMPL <122495258+AndresMPL@users.noreply.github.com>
2023-11-21 08:45:30 -0500

    updates

---
+++ 01_Manual_R.R
@@ -0,0 +1,4 @@
+#-----Manual de R
+#Freddy Hernández Olga Usuga
+#2021-07-26
+#https://uniandes-my.sharepoint.com/:b:/r/personal/am_palaciol_uniandes_edu_co/Documents/00%20-%20R/Manual%20de%20R.pdf?csf=1&web=1&e=0NraVe
---
+++ 02_Programacion_R.R
@@ -0,0 +1,5 @@
+#------El arte de programar en R: un lenguaje para la estadística
+#Julio Sergio Santana
+#Efraín Mateos Farfán
+#27 de noviembre de 2014
+#https://uniandes-my.sharepoint.com/:b:/r/personal/am_palaciol_uniandes_edu_co/Documents/00%20-%20R/Lenguaje%20Programacion%20en%20R.pdf?csf=1&web=1&e=lZWasW
